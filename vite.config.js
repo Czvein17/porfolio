ed04ee9cbbf60908262ec4dc37873123bb0b800d
Czvein Lei Unarce <intern1@highlysucceed.com>
2024-07-17 19:55:21 +0800

    Commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/Portfolio/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
 });
